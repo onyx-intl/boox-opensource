83974ef2f5dafaa72c98305dc80ec670788fdc8d
jim <qiujiejun@onyx-international.com>
2011-12-02 16:18:06 +0800

    load translator in calendar.oar. https://trakrapp.com/trakr/a#projects/boox/issues/957

--- code/src/calendar/main.cpp
+++ code/src/calendar/main.cpp
@@ -1,5 +1,7 @@
 #include <QApplication>
 
+#include "onyx/ui/ui.h"
+#include "onyx/ui/languages.h"
 #include "onyx/ui/calendar.h"
 #include "onyx/screen/screen_update_watcher.h"
 #include "onyx/sys/sys.h"
@@ -9,6 +11,9 @@ using namespace ui;
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    ui::loadTranslator (QLocale::system().name());
+
     Calendar calendar(0);
     calendar.showMaximized();
     sys::SysStatus::instance().setSystemBusy(false);
