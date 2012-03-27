a8909d059461f591e0a5ed94f497ca3c09187a5b
joy <joy@onyx-international.com>
2012-03-27 18:54:03 +0800

    change the way of getting size of Sketch Thickness when merging pdf

--- code/src/pdf_tools/src/DeviceScribbleReader.cpp
+++ code/src/pdf_tools/src/DeviceScribbleReader.cpp
@@ -206,7 +206,7 @@ static bool parseDeviceScribblePage(const sketch::PageKey &key, const sketch::Sk
             continue;
         }
 
-        const double stroke_thickness = sketch::getPointSize(it->get()->shape(), 1.0 / stroke_zoom_factor);
+        const double stroke_thickness = sketch::getPenSize(it->get()->shape()) / stroke_zoom_factor;
         const int GRAY_MAX = 0xFF;
         const double stroke_gray = sketch::getPenColor(it->get()->color()) / static_cast<double>(GRAY_MAX);
         parsedScribble.strokes_.push_back(PageScribble::Stroke(pa_points, stroke_thickness, stroke_gray));
